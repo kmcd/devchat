7d0009d32ab8237a07931351f679e1ba8b7efd59
Keith McDonnell <keith@dancingtext.com>
2013-12-10 16:28:01 +0000

    Devchat: chat for developers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Devchat::Application.config.secret_key_base = 'f5abdfb232ac6c9f2951b4d5a0d3d675a593703c7bbf8fb5342946b0d569b062ff2a810d8fbdb0bc6b73e1d86bbbc7bb8a1d1b4d5b5373592dc518f5bf56c47e'
