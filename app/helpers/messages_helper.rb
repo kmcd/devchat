ba3b9632bb194841176d9eb71dfcd1ebc651a5a1
Keith McDonnell <keith@dancingtext.com>
2013-12-17 15:03:08 +0000

    /messages/create -> image & url linking OK

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,15 @@
 module MessagesHelper
+  def markup(text)
+    return convert_single_image(text) if single_image(text)
+    auto_link text
+  end
+  
+  def convert_single_image(text)
+    return unless image = single_image(text)
+    link_to(image) { image_tag image }
+  end
+  
+  def single_image(text)
+    text[/^(http\S+(?:jpe?g|gif|png))(\?.*)?$/i]
+  end
 end
